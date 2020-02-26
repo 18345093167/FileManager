a7e887e3c178eddf8e47cb25675b5c4c156a342c
BurningTeng <2250911301@qq.com>
2020-02-26 21:19:19 +0800

    add register but not use database to save users

--- src/main/java/com/example/uploadingfiles/LoginController.java
+++ src/main/java/com/example/uploadingfiles/LoginController.java
@@ -2,35 +2,51 @@ package com.example.uploadingfiles;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.websocket.server.PathParam;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping(value = "/toLogin")
-    public String toLogin(@PathParam(value = "username") String username,
-            @PathParam(value = "password") String password,
-            Map<String, Object> map, HttpSession session) {
-        
-        if(!StringUtils.isEmpty(username)&& username.equals("burning")&&"123456".equals(password)){
+    @RequestMapping(value = "/toLogin", method = RequestMethod.POST)
+    public String toLogin(@RequestParam(value = "username") String username,
+            @RequestParam(value = "password") String password, Map<String, Object> map, HttpSession session) {
+
+        if (!StringUtils.isEmpty(username) && username.equals("burning") && "123456".equals(password)) {
             session.setAttribute("loginUser", username);
-            return "redirect:/";
+            return "test";
         } else {
             map.put("msg", "用户名或密码错误");
             return "index";
         }
     }
+
+    @RequestMapping(value = "/toRegister", method = RequestMethod.POST)
+    public String toRegister(HttpServletRequest request,RedirectAttributes redirectAttributes) {
+        String email = request.getParameter("email");
+        String userName = request.getParameter("username");
+        String passWd = request.getParameter("password2");
+        return "redirect:/login";
+
+    }
     @GetMapping(value="/login")
     public String login(Model model) {
         model.addAttribute("msg", "登陆后可上传私密文件");
         return "index";
+    }
+    @GetMapping(value="/register")
+    public String register(Model model) {
+        model.addAttribute("msg", "注册后享专属文件系统");
+        return "register";
     }
     
 }
--- src/main/java/com/example/uploadingfiles/config/LoginInterceptor.java
+++ src/main/java/com/example/uploadingfiles/config/LoginInterceptor.java
@@ -12,15 +12,14 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request,
             HttpServletResponse response, Object handler) throws Exception {
-      /*  HttpSession session = request.getSession();
+        HttpSession session = request.getSession();
         Object user = session.getAttribute("loginUser");
-        if(user == null){
+        if(user == null && request.getRequestURI().contains("login")){
         	request.setCharacterEncoding("UTF-8");
             request.setAttribute("msg", "用户名为空");
             request.getRequestDispatcher("/login").forward(request, response);
             return false;
-        }*/
-        
+        }
         return true;
     }
 
